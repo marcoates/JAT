40b61db811c5bd1577a1e65d802d226e8d1009d1
Marco Braggion <Marco@MacBook-Pro-di-Marco-2.local>
2024-01-26 21:18:54 +0100

    Fixing routing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+
+  const basename = 'http://marcoates.github.io/JAT-Job-Application-Tracker'; 
+
   return (
-    <HashRouter>
+    <HashRouter basename={basename}>
     <Header />
     <Routes>
      <Route path="/" element={<Home />} />
